46665e6329e0ae6a237f4a04f92cd5569a8f4052
Guillermo Haserfaty <guillermo.haserfaty@bairesdev.com>
2019-07-02 18:55:37 -0300

    spaces deleted

--- coach-app/src/utils/validations.js
+++ coach-app/src/utils/validations.js
@@ -4,14 +4,10 @@ import {groupBy} from 'lodash';
 export const isPlayersDataValid = (playersData,teamSize) => {
     if(!Array.isArray(playersData) || playersData.length !== (teamSize * 2)) return false;
 
-    let teams = groupBy(playersData,'team.id')
+    let teams = groupBy(playersData,'team.id');
     if(teams[CONSTANTS.BLUE_TEAM].length === teamSize && teams[CONSTANTS.RED_TEAM].length === teamSize){
-        return true
+        return true;
     }
-
-
     return false;
-
-
 }
 
